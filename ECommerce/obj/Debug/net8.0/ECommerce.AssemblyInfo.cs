0b6e9858cf1a5e2bb042233302ac01d39983daa4
Pendik <omer@gmail.com>
2025-05-09 12:31:55 +0300

    gorev

--- ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8261cac7b1667640a154e01ca4d8f78a4dd2460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f402c55033cf1eeea1251ce408ca31ffa1cb535e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
